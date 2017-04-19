f41019530785f8e995de2f842da65f8cd3a571ae
Sagar Dhage <sagard147@gmail.com>
2017-04-19 20:35:43 +0530

    Update Parallal_code.cpp

--- pc1/Parallal_code.cpp
+++ pc1/Parallal_code.cpp
@@ -1,20 +1,40 @@
-#include <iostream>
+ #include <iostream>
 #include <cstring>
 #include <omp.h>
 
 using namespace std;
 
+/* N is the maximum string length */
 
 #define N 600
 
-
+/* Cost function f(m, n). During
+ * testing, I will define my own
+ * cost function which will NOT be
+ * the same as this function!
+ */
 
 int f(int m, int n) {
   return ((m+1)*(n+1)- 1) % N;
 }
 
 
-int min(int x, int y, int z) {
+/* The global variable M[][] is a
+ * 2-D table to compute the edit-
+ * distance between two strings x
+ * and y. Specifically, M[i][j] is
+ * the edit distance between x[i..]
+ * and y[j..]. The table entries
+ * are initially undefined (-1).
+ *
+ * The notation x[i..] denotes the
+ * sub-string of x starting from i.
+ */
+int M[N+1][N+1];
+ 
+/* Helper function to compute the
+   minimum of three ints: x, y, z */
+ int min(int x, int y, int z) {
   if(x < y) {
     return x < z ? x : z;
   }
@@ -28,44 +48,65 @@ int min(int x, int y, int z) {
 
 int editDistance(char *x, char *y) {
 int i,j,cost;
+/*
+ * 2-D table to compute the edit-distance between two strings x and y. 
+ * Specifically, M[i][j] is the edit distance between x[i..] and y[j..].
+ *  The notation x[i..] denotes the
+ * sub-string of x starting from i.
+ **we initialized that matrix locally to avoid race condition 
+ */
 int M[N+1][N+1];
+
+//getting the length of 1st string and 2nd string 
 int X=strlen(x);
 int Y=strlen(y);
 
-
-#pragma omp parallel 
-{
  	
  	for(i = 0; i <= Y-1; i++) 
 	{	
+		// First string is empty
+        // where we should isnert all characters of second string
+		// so Min. operations = Y-1
 	   M[X][i]=(Y)-(i); 
 	}
        
-	
-	
 	for(i = 0; i <= X; i++) 
 	{
-	   M[i][Y]=(X)-(i); 
+		// Second string is empty
+        // where we should add all characters of first
+		// Min. operations = X-i
+	    M[i][Y]=(X)-(i); 
 	}
         
-}
-
-
- 		for(i = X-1; i>=0; i--) {
+		/*
+		The above statement means that to reach cell (i,j) wit minimum cost,
+		first reach either cell(i-1,j) or cell (i,j-1) in as minimum cost as possible.
+		From there, jump to cell (i,j). 
+		This brings us to the two important conditions which need to be satisfied for a dynamic programming problem:
+			Optimal Sub-structure:- Optimal solution to a problem involves optimal solutions to sub-problems.
+			Overlapping Sub-problems:- Subproblems once computed can be stored in a table for further use. 
+										This saves the time needed to compute the same sub-problems again and again.
+		*/
+		for(i = X-1; i>=0; i--) {
     		for(j = Y-1; j>=0; j--) {
      		cost = f(i,j);
        		M[i][j]=min(cost + M[i+1][j],cost + M[i][j+1],(x[i] != y[j])*cost +M[i+1][j+1]);
  		   }
  		 }
-
+/* Return the edit cost distance between
+   * x[0..] == x and y[0..] == y.
+*/
 return M[0][0];
 }
 
+//main function call, where we have to pass the strings as the arguments to find the edit distance   
 int main(int argc, char **argv) {
   if(argc <= 2) {
     cout << "Usage: " << argv[0] << " str1 str2 ..." << endl;
     return 1;
   }
+  
+  // checking the length of each strings to allocated length    
   for(int i = 1; i < argc; i++) {
     if(strlen(argv[i]) > N) {
       cout << "Maximum string length: " << N << endl;
@@ -74,6 +115,8 @@ int main(int argc, char **argv) {
   }
   
   int total = 0;
+  // for each pair of Strings we are calling editDistance() function to calculate the minimum edit cost.
+  // we are making this function call to be happen in parallel for each pair of string separately
   #pragma omp parallel for reduction(+:total)
   for(int i = 1; i < argc-1; i++) {
     for(int j = i+1; j < argc; j++) {
