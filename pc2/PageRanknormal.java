963983012e892df19e3ec078bc3bd0c7d6582a8b
Sagar Dhage <sagard147@gmail.com>
2017-04-20 15:28:44 +0530

    Update PageRanknormal.java

--- pc2/PageRanknormal.java
+++ pc2/PageRanknormal.java
@@ -1,80 +1,47 @@
 import java.util.*;
-
 import java.io.*;
 
 public class PageRank 
 {
-
-    public int path[][] = new int[10][10];
-
-    public double pagerank[] = new double[10];
+   public int path[][] = new int[10][10];
+   public double pagerank[] = new double[10];
 
   
 
     public void calc(double n)
-
     {   
+	    double init;
+	    double c=0;
+	    double temp[] = new double[10];
+ 	    int i,j,u=1,k=1;
 
- 	double init;
-
- 	double c=0;
-
-
-
-        double temp[] = new double[10];
- 	int i,j,u=1,k=1;
-
- 	init = 1/n;
-
- //System.out.printf(" n value:"+n+"\t init value :"+init+"\n");
-
- for(i=1;i<=n;i++)
-
-   this.pagerank[i]=init;
-
-   /*System.out.printf("\n Initial PageRank Values , 0th Step \n");
+ 	    init = 1/n;
 
     for(i=1;i<=n;i++)
-
-   System.out.printf(" Page Rank of "+i+" is :\t"+this.pagerank[i]+"\n");*/
-
-   
-
-   
+      this.pagerank[i]=init;
+ 
 
    while(u<=8)
-
    {
+	for(i=1;i<=n;i++)
+	{  
+		temp[i]=this.pagerank[i];
+		this.pagerank[i]=0;
 
-    for(i=1;i<=n;i++)
-
-    {  temp[i]=this.pagerank[i];
-
-          this.pagerank[i]=0;
-
-       }
+        }
 
      
 
- for(j=1;j<=n;j++)
-
-        for(i=1;i<=n;i++)
-
-     if(this.path[i][j] == 1)
-
-     {  k=1;c=0;
-
-           while(k<=n)
-
-     {
-
-       if(this.path[i][k] == 1 )
-
-                   c=c+1;
-
-     k=k+1;
-
-     }
+ 	for(j=1;j<=n;j++)
+	     for(i=1;i<=n;i++)
+		if(this.path[i][j] == 1)
+		 {  k=1;c=0;
+		while(k<=n)
+		{
+		if(this.path[i][k] == 1 )
+			c=c+1;
+     			k=k+1;
+		}
 
              this.pagerank[j]=this.pagerank[j]+temp[i]*(1/c);   
 
@@ -156,11 +123,7 @@ public class PageRank
 	 
 	 
 
-        p.calc(nodes);
-
-    
-
-           
+        p.calc(nodes);     
 
     }  
 
